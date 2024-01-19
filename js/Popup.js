479bd456a274be49f2600fd43df6c4fcfc37d4ab
mohamed refat <mohammref312@gmail.com>
2024-01-19 02:47:27 +0200

    project section modeified

--- js/Popup.js
+++ js/Popup.js
@@ -3,33 +3,25 @@ $(document).ready(function() {
   // Gets the video src and the description from the data attributes
   var videoSrc;
   var description;
+  var title;
   $('.video').click(function() {
     videoSrc = $(this).data("video");
     description = $(this).data("description");
+    title = $(this).data('title');
   });
   
   // Assigns the video src and the description to the iframe and the p element when the modal is opened
   $('#videoModal').on('shown.bs.modal', function (e) {
     $("#videoModal iframe").attr('src', videoSrc + "?autoplay=1&modestbranding=1&showinfo=0"); 
     $("#videoModal p").text(description);
+    $("#videoModal h5").text(title);
   });
     
   // Removes the video src and the description when the modal is closed
   $('#videoModal').on('hide.bs.modal', function (e) {
     $("#videoModal iframe").attr('src', ""); 
     $("#videoModal p").text("");
+    $("#videoModal h5").text("");
   }); 
-});
-
-
-
-$(document).ready(function() {
-  $('.project').click(function() {
-    var title = $(this).find('.text h3').text();
-    var description = $(this).find('.text p').text();
-
-    $('#project-modal-title').text(title);
-    $('#project-modal-description').text(description);
-  });
 });
 })(jQuery);
